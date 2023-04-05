8f94d0a0c2c76d2ac312099a33694c0a45ecdd03
nashwanchowdhury <120747770+nashwanchowdhury@users.noreply.github.com>
2023-04-05 12:21:48 -0400

    new files

---
+++ mapsAndSets.js
@@ -0,0 +1,28 @@
+let fruitInventory = new Map();
+
+fruitInventory.set('apple', 10);
+fruitInventory.set('banana', 20);
+fruitInventory.set('orange', 30);
+
+fruitInventory.set('apple', 15);
+
+let bananaInventory = fruitInventory.get('banana');
+
+let uniqueColors = new Set();
+
+uniqueColors.add('red');
+uniqueColors.add('blue');
+uniqueColors.add('green');
+
+uniqueColors.add('yellow');
+
+let hasBlue = uniqueColors.has('blue');
+
+
+
+
+
+
+
+
+
