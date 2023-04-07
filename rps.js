936f5b456d802ad49dea65afa913d002ca9e30ad
nashwanchowdhury <120747770+nashwanchowdhury@users.noreply.github.com>
2023-04-07 18:47:34 -0400

    new files

--- rps.js
+++ rps.js
@@ -1,47 +1,125 @@
+let score1 = 0;
+let score2 = 0;
+
 const hands = ['rock', 'paper', 'scissors'];
 
-let player1 = {
+var player1 = {
     name: 'Nash',
-    choice: choice1()
+    getHand: function () {
+        return(hands[parseInt(Math.random()*10)%3]);
+    }
 }
-let player2 = {
+var player2 = {
     name: 'Bob',
-    choice: choice2()
-}
-function getHand() {
-    return hands[parseInt(Math.random()*10)%3];
-}
-   
-function choice1() {
-    return getHand();   
-}
-
-function choice2() {
-    return getHand();
-}
-
-
-function playRound(player1, player2) {
-
-const win1 = (player1.choice == 'paper' && player2.choice == 'rock') || (player1.choice == 'rock' && player2.choice == 'scissors') || (player1.choice == 'scissors' && player2.choice == 'paper');
-const win2 = (player1.choice == 'paper' && player2.choice == 'scissors') || (player1.choice == 'rock' && player2.choice == 'paper') || (player1.choice == 'scissors' && player2.choice == 'rock');
-
-if (player1.choice == player2.choice) {
-    console.log(player1.name + ': ' + player1.choice);
-    console.log(player2.name + ': ' + player2.choice);
-    console.log('This is a tie!');
-}
-else if (win1) {
-    console.log(player1.name + ': ' + player1.choice);
-    console.log(player2.name + ': ' + player2.choice);
-    console.log(player1.name + " wins");
-} else if (win2) {
-    console.log(player1.name + ': ' + player1.choice);
-    console.log(player2.name + ': ' + player2.choice);
-    console.log(player2.name + " wins");
-} else {
+    getHand: function () {
+        return hands[parseInt(Math.random()*10)%3];
+        }
     
 }
+var player3 = {
+    name: 'Devin',
+    getHand: function () {
+        return hands[parseInt(Math.random()*10)%3];
+        }
+    
+}
+var player4 = {
+    name: 'Bonnie',
+    getHand: function() {
+        return hands[parseInt(Math.random()*10)%3];
+        }
+    
+}
+function playRound(p1, p2) {
 
+choice1 = p1.getHand();
+choice2 = p2.getHand();
+
+
+const win1 = (choice1 == 'paper' && choice2 == 'rock') || (choice1 == 'rock' && choice2 == 'scissors') || (choice1 == 'scissors' && choice2 == 'paper');
+const win2 = (choice1 == 'paper' && choice2 == 'scissors') || (choice1 == 'rock' && choice2 == 'paper') || (choice1 == 'scissors' && choice2 == 'rock');
+
+if (choice1 == choice2) {
+     console.log(p1.name + ': ' + choice1);
+     console.log(p2.name + ': ' + choice2);
+     console.log('This is a tie!');
+}
+else if (win1) {
+    score1++;
+    console.log(p1.name + ': ' + choice1);
+    console.log(p2.name + ': ' + choice2);
+    console.log(p1.name + ' wins!');
+} else {
+    score2++;
+    console.log(p1.name + ': ' + choice1);
+    console.log(p2.name + ': ' + choice2);
+    console.log(p2.name + ' wins!');
+}
 };
-playRound(player1, player2);
+
+//playRound(player1, player2);
+
+function playGame (p1, p2, playUntil) {
+    if (score1 == playUntil) {
+        console.log(p1.name + " wins the best of 5!");
+        score2 = 0;
+        score1 = 0;
+        p1.winner = true; 
+    } else if (score2 == playUntil) {
+        console.log(p2.name + " wins the best of 5!");
+        score2 = 0;
+        score1 = 0;
+        p2.winner = true; 
+    } else {
+        playRound(p1, p2);
+        playGame(p1, p2, playUntil);
+    }
+
+}
+//playGame(player1, player2, 5);
+function playTournament (p1, p2, p3, p4, playUntil) {
+    
+    playGame(p1, p2, playUntil);
+    playGame(p3, p4, playUntil);
+   
+    if (p1.winner == true && p3.winner == true) {
+        playGame(p1, p3, playUntil);
+        p1.winner = false;
+        p2.winner = false;
+        p3.winner = false;
+        p4.winner = false; 
+    } else if (p1.winner == true && p4.winner == true) {
+        playGame(p1, p4, playUntil);
+        p1.winner = false;
+        p2.winner = false;
+        p3.winner = false;
+        p4.winner = false;
+    } else if (p2.winner == true && p3.winner == true) {
+        playGame(p2, p3, playUntil);
+        p1.winner = false;
+        p2.winner = false;
+        p3.winner = false;
+        p4.winner = false; 
+    } else {
+        playGame(p2, p4, playUntil);
+        p1.winner = false;
+        p2.winner = false;
+        p3.winner = false; 
+        p4.winner = false; 
+    }
+
+    if (p1.winner == true) {
+        console.log(p1.name + ' is the world champion!')
+    } else if (p2.winner == true) {
+        console.log(p2.name + ' is the world champion!')
+    } else if (p3.winner == true) {
+        console.log(p3.name + ' is the world champion!')
+    } else if (p4.winner == true) {
+        console.log(p4.name + ' is the world champion!')
+    } else 
+        return 1;
+
+}
+
+playTournament(player1, player2, player3, player4, 5);
+
